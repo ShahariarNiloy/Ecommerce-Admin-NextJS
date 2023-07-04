b55d1416cae53c6a33bdc66120cc0847be949f3c
ShahariarNiloy <shahariarniloyblv@gmail.com>
2023-07-04 10:04:07 +0600

    store modal

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,13 +2,14 @@ import { ClerkProvider } from "@clerk/nextjs";
 import "./global.css";
 import { Inter } from "next/font/google";
 import { ModalProvider } from "@/providers/ModalProvider";
+import { ToastProvider } from "@/providers/ToastProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Ecommerce - Admin",
   description:
-    "An admin dashboard for ecommerce websites. It will work as a CMS.",
+    "An admin dashboard for e-commerce websites. It will work as a CMS.",
 };
 
 export default function RootLayout({
@@ -20,6 +21,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
+          <ToastProvider />
           <ModalProvider />
           {children}
         </body>
