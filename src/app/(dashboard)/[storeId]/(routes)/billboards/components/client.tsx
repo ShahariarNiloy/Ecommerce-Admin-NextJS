c5f08e28a599d8ea54b39c49136bfd49631de32f
ShahariarNiloy <shahariarniloyblv@gmail.com>
2023-07-05 17:03:21 +0600

    billboard page don

---
+++ src/app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { Plus } from "lucide-react";
+import { useParams, useRouter } from "next/navigation";
+
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+
+import { columns, BillboardColumn } from "./columns";
+import { DataTable } from "@/components/ui/DataTable";
+import { Heading } from "@/components/ui/Heading";
+import { ApiList } from "@/components/ui/ApiList";
+
+interface BillboardClientProps {
+  data: BillboardColumn[];
+}
+
+export const BillboardClient: React.FC<BillboardClientProps> = ({ data }) => {
+  const params = useParams();
+  const router = useRouter();
+
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <Heading
+          title={`Billboards (${data.length})`}
+          description="Manage billboards for your store"
+        />
+        <Button
+          onClick={() => router.push(`/${params.storeId}/billboards/new`)}
+        >
+          <Plus className="mr-2 h-4 w-4" /> Add New
+        </Button>
+      </div>
+      <Separator />
+      <DataTable searchKey="label" columns={columns} data={data} />
+      <Heading title="API" description="API Calls for Billboards" />
+      <Separator />
+      <ApiList entityName="billboards" entityIdName="billboardId" />
+    </>
+  );
+};
